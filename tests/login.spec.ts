4e3586046d0759f84374113640a5d1551e08ebd2
Lexsak <bartoszpiprek123@gmail.com>
2023-11-16 22:10:52 +0100

    First aaa implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,22 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("User login to Demobank", () => {
-  test("login with correct credentials", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("tester12");
-    await page.getByTestId("password-input").fill("aaaaaaaa");
+  test.only("login with correct credentials", async ({ page }) => {
+    // Arrange
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "tester12";
+    const userPassword = "aaaaaaaa";
+    const expectedUserName = "Jan Demobankowy";
+    
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
     await page.getByTestId("user-name").click();
 
-    await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+    // Assert
+    await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
   });
 
   test("unsuccessful login with too short username", async ({ page }) => {
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,25 +1,37 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Group description", () => {
-  test("quick payment with correct data", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("tester12");
-    await page.getByTestId("password-input").fill("aaaaaaaa");
+  test.only("quick payment with correct data", async ({ page }) => {
+    // Arrange
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "tester12";
+    const userPassword = "aaaaaaaa";
+
+    const receiverId = "2";
+    const transferAmount = "150";
+    const transferTitle = "pizza";
+    const expectTransferReceiver = "Chuck Demobankowy";
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
+    await page.getByTestId("user-name").click();
 
-    await page.locator("#widget_1_transfer_receiver").selectOption("2");
-    await page.locator("#widget_1_transfer_amount").fill("150");
-    await page.locator("#widget_1_transfer_title").fill("pizza");
+    await page.locator("#widget_1_transfer_receiver").selectOption(receiverId);
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
 
-    // await page.getByRole("button", { name: "wykonaj" }).click();
     await page.locator("#execute_btn").click();
     await page.getByTestId("close-button").click();
     // await page.getByRole("link", {
     //   name: "Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza",
     // }).click();
 
+    // Assert
     await expect(page.locator("#show_messages")).toHaveText(
-      "Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza"
+      `Przelew wykonany! ${expectTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
 
